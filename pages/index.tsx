258e991081872f4c74e39ea3277dbf57088717e0
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2023-04-05 18:48:06 -0600

    Update index.tsx
    toning

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,7 +63,7 @@ const Home: NextPage = () => {
           <ModalBody textAlign="center">
             <Heading size="lg" mb={4} color="white">Safety First!</Heading>
             <Text color="white">The Phantom link on mobile takes you to download the app. Open this webpage from your Phantom browser to connect your wallet. On desktop, after installing Phantom, or any wallet app, you will be prompted to create a wallet or import one with a seed phrase. <b>Consider setting up your wallet in a new browser window to isolate your extension, which can enhance user confidence.</b><br></br><br></br> The wallet pop-up window is your private interface with the associated blockchain, entering your seed phrase grants access to approve transactions. Double-check addresses to avoid man-in-the-middle attacks that may authorize third-party transactions from your wallet.
-            <br></br><br></br><i>This mint UI is still in beta, you were probably sent this link directly by Tanner, Founder of HeliumDenver. You will be delivered your sensor in person at the time of your course. Find us in the Discord for any questions!</i></Text><br></br>
+            <br></br><br></br><i>HACKSs NFTs is brought to you by HeliumDenver. Each NFT represents the purchase of a Helium sensor and in-person onboarding hacks course. You will be delivered your sensor in person at the time of your course. Find us in the Discord for any questions!</i></Text><br></br>
             <Button onClick={() => setShowHelp(false)}>Close</Button>
           </ModalBody>
 
