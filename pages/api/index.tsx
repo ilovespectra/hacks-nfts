601487cec647edc2415ef2e68ffa3d06f3943a07
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2023-04-08 20:00:12 -0600

    Update index.tsx
    repair

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -1,86 +1,32 @@
-import { Box, 
-  Center, 
-  Spacer, 
-  Stack, 
-  Modal, 
-  ModalOverlay, 
-  ModalContent, 
-  ModalHeader, 
-  ModalBody, 
-  ModalFooter,
-  Heading,
-  Text
-} from "@chakra-ui/react"
-import type { NextPage } from "next"
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
-import NavBar from "../components/NavBar"
-import Disconnected from "../components/Disconnected"
-import { connected } from "process"
-import { useWallet } from "@solana/wallet-adapter-react"
-import Connected from "../components/Connected"
-import { useState } from "react";
-import { Button, VStack } from "@chakra-ui/react";
-
-const Home: NextPage = () => {
-  const {connected} = useWallet()
-  const [showHelp, setShowHelp] = useState(false);
   return (
     <div className={styles.container}>
       <Head>
-        <title>LavaDAO Philanthropy Token</title>
-        <meta name="HeliumDenver, by LavaDAO" />
+        <title>Hacks NFTs</title>
+        <meta name="HACKs NFTs by HeliumDenver" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Box
         w="full"
-        h="160vh"
+        h="130vh"
         bgImage={`url(${'/images/home-background.png'})`}
         backgroundSize="cover"
         backgroundPosition="center"
-        className="blur-background"
-      >
-        
-        <Stack w="full" h="100vh" justify="center">
-          <NavBar />
-
-          <Spacer />
-          <Center>
-            { connected ? <Box
-                            backgroundImage="url('/images/background.jpg')"
-                            backgroundRepeat="no-repeat"
-                            backgroundSize="cover"
-                          >
-                        <Connected />
-                      </Box> : <Disconnected /> }
-          </Center>
-          <Spacer />
-          {showHelp && (
-        <div>
-          <Modal isOpen={showHelp} onClose={() => setShowHelp(false)} isCentered>
+	@@ -61,10 +61,10 @@ const Home: NextPage = () => {
         <ModalOverlay />
         <ModalContent bg="rgba(0, 0, 0, 0.5)">
           <ModalBody textAlign="center">
-            <Heading size="lg" mb={4} color="white">Glad You're Here!</Heading>
-            <Text color="white">Here's your chance to be a part of something big. We're creating a Helium expo and hackathon, big dreams write big checks, and we could use your help.
-            <br></br><br></br><i>LavaDAO is a Colorado LCA created to manage HeliumDenver. Help us acheive our lofty goals by purchasing a Philanthropy NFT. Simple as it sounds, provides immediate equity to LavaDAO for HeliumDenver marketings and events.</i></Text><br></br>
-            <Button onClick={() => setShowHelp(false)}>Done</Button>
+            <Heading size="lg" mb={4} color="white">Safety First!</Heading>
+            <Text color="white">The Phantom link on mobile takes you to download the app. Open this webpage from your Phantom browser to connect your wallet. On desktop, after installing Phantom, or any wallet app, you will be prompted to create a wallet or import one with a seed phrase. <b>Consider setting up your wallet in a new browser window to isolate your extension, which can enhance user confidence.</b><br></br><br></br> The wallet pop-up window is your private interface with the associated blockchain, entering your seed phrase grants access to approve transactions. Double-check addresses to avoid man-in-the-middle attacks that may authorize third-party transactions from your wallet.
+            <br></br><br></br><i>HACKSs NFTs is brought to you by HeliumDenver. Each NFT represents the purchase of a Helium sensor and in-person onboarding hacks course. You will be delivered your sensor in person at the time of your course. Find us in the Discord for any questions!</i></Text><br></br>
+            <Button onClick={() => setShowHelp(false)}>Close</Button>
           </ModalBody>
 
         </ModalContent>
-      </Modal>
-        </div>
-        )}
-
-          <Center>
-            <Box
-              p={8}
-              borderRadius="xl"
-              bg="rgba(0, 0, 0, 0.5)"
+	@@ -80,7 +80,7 @@ const Home: NextPage = () => {
               backdropFilter="blur(15px)"
             >
               <Box marginBottom={1} color="black" fontSize="4xl" textAlign="center">
-              <Button onClick={() => setShowHelp(true)}>   ?   </Button>
+              <Button onClick={() => setShowHelp(true)}>Want Help?</Button>
               </Box>
               <Box marginBottom={1} color="white" fontSize="xl" textAlign="center">
                 <a
@@ -98,5 +44,4 @@ const Home: NextPage = () => {
     </div>
   )
 }
-
 export default Home
