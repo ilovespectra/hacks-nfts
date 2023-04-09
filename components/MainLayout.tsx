0742d0fdcaa6b125ad8eb4cfd7d20e6b5c80fbcd
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2023-04-08 19:59:00 -0600

    Update MainLayout.tsx
    repair

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -4,41 +4,21 @@ import styles from "../styles/Home.module.css"
 import { Box, Center, Spacer, Stack } from "@chakra-ui/react"
 import NavBar from "../components/NavBar"
 import { useWallet } from "@solana/wallet-adapter-react"
-
 const MainLayout: FC<{ children: ReactNode }> = ({ children }) => {
   const { connected } = useWallet()
-
   return (
     <div className={styles.container}>
       <Head>
-        <title>LavaDAO Presents the HeliumDenver Philanthropy Token</title>
-        <meta name="The NFT Collection for philanthropists within the Helium ecosystem." />
+        <title>HeliumDenver Presents the Hacks NFT Collection</title>
+        <meta name="The NFT Collection for HeliumDenver Hacks sessions" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Box
-        w="full"
-        h="calc(100vh)"
-        bgImage={connected ? "" : "url(/background.svg)"}
-        backgroundPosition="center"
-      >
-        <Stack w="full" h="calc(100vh)" justify="center">
-          <NavBar />
-
-          <Spacer />
-
-          <Center>{children}</Center>
-
-          <Spacer />
-
-          <Center>
-            <Box marginBottom={4} color="white">
-              <a
-                href="https://twitter.com/heliumdenver"
+	@@ -38,7 +38,7 @@ const MainLayout: FC<{ children: ReactNode }> = ({ children }) => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Big things are rising. 
+                Hacks for the masses. 
               </a>
             </Box>
           </Center>
@@ -47,5 +27,4 @@ const MainLayout: FC<{ children: ReactNode }> = ({ children }) => {
     </div>
   )
 }
-
 export default MainLayout
