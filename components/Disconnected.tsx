bb22ce5366449c202c6a5c769146d269c6436124
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2023-04-08 19:58:27 -0600

    Update Disconnected.tsx
    repair

--- components/Disconnected.tsx
+++ components/Disconnected.tsx
@@ -1,67 +1,29 @@
-import { FC, MouseEventHandler, useCallback } from "react"
-import {
-  Button,
-  Container,
-  Heading,
-  HStack,
-  Text,
-  VStack,
-  Box,
-} from "@chakra-ui/react"
-import { ArrowForwardIcon } from "@chakra-ui/icons"
-import { useWalletModal } from "@solana/wallet-adapter-react-ui"
-import { useWallet } from "@solana/wallet-adapter-react"
-
-
-const Disconnected: FC = () => {
-  const modalState = useWalletModal()
-  const { wallet, connect } = useWallet()
-
-  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
-    (event) => {
-      if (event.defaultPrevented) {
-        return
-      }
-
-      if (!wallet) {
-        modalState.setVisible(true)
-      } else {
-        connect().catch(() => {})
-      }
-    },
-    [wallet, connect, modalState]
-  )
-
-  return (
+ return (
     <>
       <Container>
-        <VStack spacing={10}>
+        <VStack spacing={20}>
           <Heading
             color="white"
             as="h1"
             size="4xl"
-            noOfLines={4}
+            noOfLines={3}
             textAlign="center"
           >
           <Box
           bgColor="rgba(0, 0, 0, 0.6)"
           borderRadius="20px"
-          p={5}
+          p={8}
           w="100%"
           maxW="800px"
           mt={{ base: "30%", md: "10%" }}
           textAlign="center" // added textAlign property
           fontSize={{ base: "2xl", md: "4xl" }}
         >
-            Feeling<br />
-            Philanthropic?
-
-            <Box textAlign="center" // added textAlign property
-          fontSize={{ base: "sm", md: "l" }}><br />HeliumDenver - LavaDAO</Box>
-            
+            Mint HACKs.<br />
+            Learn skills.<br />
+            Level up.
             </Box>
           </Heading>
-          
           <Button
             bgColor="blue"
             color="white"
@@ -69,7 +31,7 @@ const Disconnected: FC = () => {
             onClick={handleClick}
           >
             <HStack>
-              <Text>Join LavaDAO</Text>
+              <Text>Become a Hacker</Text>
               <ArrowForwardIcon />
             </HStack>
           </Button>
@@ -78,5 +40,4 @@ const Disconnected: FC = () => {
     </>
   )
 }
-
 export default Disconnected
