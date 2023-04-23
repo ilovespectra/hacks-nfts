effd2543665fa903a98dfd24cdd49b1ea52db441
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2023-04-23 11:29:31 -0600

    Update Disconnected.tsx
    fixin'

--- components/Disconnected.tsx
+++ components/Disconnected.tsx
@@ -4,6 +4,7 @@ import {
   Container,
   Heading,
   HStack,
+  Image,
   Text,
   VStack,
   Box,
@@ -11,6 +12,8 @@ import {
 import { ArrowForwardIcon } from "@chakra-ui/icons"
 import { useWalletModal } from "@solana/wallet-adapter-react-ui"
 import { useWallet } from "@solana/wallet-adapter-react"
+import "typeface-inter";
+
 
 
 const Disconnected: FC = () => {
@@ -35,51 +38,61 @@ const Disconnected: FC = () => {
   return (
     <>
       <Container>
-      <div style={{
-  backgroundImage: "url('/images/hacks.png')",
-  width: "100%",
-  height: "100%",
-  backgroundSize: "cover",
-  backgroundRepeat: "no-repeat",
-  backgroundPosition: "center center",
-  border: "none",
-  margin: 0,
-  padding: 0,
-}}>
-  <img src="/images/hacks.png" alt="hacks" />
-</div>
-        <VStack spacing={20}>
+      <Box
+  backgroundImage="url('/images/hacks.png')"
+  width="100%"
+  height="100%"
+  backgroundSize="cover"
+  backgroundRepeat="no-repeat"
+  backgroundPosition="center center"
+  border="none"
+  margin={0}
+  padding={0}
+  my={2}
+  mx="auto"
+>
+<Image
+  src="/images/hacks.png"
+  alt="hacks"
+  width="100%"
+  height="100%"
+  objectFit="cover"
+  />
+</Box>
+<Box><Text align="center" fontFamily="Inter">HeliumDenver presents the HACKs NFT collection. Each of these NFTs represents the purchase of a sensor and ticket to an IRL onboarding hacks session with HeliumDenver.</Text></Box>
+        
+        <VStack spacing={10}>
           <Heading
             color="white"
             as="h1"
-            size="xl"
-            noOfLines={3}
+            size="4xl"
+            noOfLines={4}
             textAlign="center"
           >
           <Box
+          bgImage="/images/eruption.gif"
+          bgSize="cover"
           bgColor="rgba(0, 0, 0, 0.6)"
           borderRadius="20px"
-          p={8}
+          p={10}
           w="100%"
           maxW="800px"
-          mt={{ base: "30%", md: "10%" }}
-        >
-            Mint HACKs.<br />
-            Learn skills.<br />
-            Level up.
-            </Box>
-          </Heading>
-          <Button
-            bgColor="blue"
+          mt={{ base: "20%", md: "10%" }}
+          textAlign="center"
+          fontSize={{ base: "2xl", md: "4xl" }}
+        ><Button
+            bgColor="black"
             color="white"
-            maxW="380px"
+            maxW="400px"
             onClick={handleClick}
           >
             <HStack>
-              <Text>Become a Hacker</Text>
+              <Text> Enter </Text>
               <ArrowForwardIcon />
-            </HStack>
+            </HStack> 
           </Button>
+          </Box>
+          </Heading>
         </VStack>
       </Container>
     </>
